1bccec419f440ef4ee2701f4ae5a6aa36f864bd7
raubenw <91202958+raubenw@users.noreply.github.com>
2022-05-01 22:16:40 +0200

    Declared constants and define pseudo code

---
+++ script.js
@@ -0,0 +1,19 @@
+const textwrapper = document.querySelector('.text-wrapper');
+const textArea = document.querySelector('#textArea');
+const originText = document.querySelector('.text');
+const resetButton = document.querySelector('#reset');
+const theTimer = document.querySelector('.timer');
+
+//Add leding zero to numbers 8
+
+// Create a clock 
+
+// Reset everything
+
+// Event listeners for keyboard input and reset button events
+
+// Start the timer
+
+// Match the text with the provided text on the page
+
+// 
